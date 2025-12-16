e85138ce070db18ab8f9190dcbab070a5492f752
Ayoub <gouskarayoub@gmail.com>
2025-12-16 16:28:19 +0100

    setting ut the books page and controlles

--- book-review/routes/web.php
+++ book-review/routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/hello/{name}', function ($name) {
+    return 'hello '. $name;
+});
+
+
+Route::resource('books', BookController::class);
+
